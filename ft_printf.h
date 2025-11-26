46a8b2ce8aa57a197ca21dc874a764434117b6cb
Snow <snow@Snow.localdomain>
2025-11-27 00:22:36 +0100

    update

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojin <ojin@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: snowp <snowp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 11:09:18 by ojin              #+#    #+#             */
-/*   Updated: 2025/11/21 11:43:52 by ojin             ###   ########.fr       */
+/*   Updated: 2025/11/27 00:12:09 by snowp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 int		ft_printf(const char *fmt, ...);
 size_t	ft_putchar(size_t c);
 size_t	ft_putstr(const char *str);
+int		ft_putptr(void *ptr);
 int		ft_lower_nbr(long n, int base);
 int		ft_upper_nbr(long n, int base);
 int		ft_format(char sp, va_list arg);
