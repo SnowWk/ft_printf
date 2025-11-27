35c8a4fdbd8efe28343019d6ec202875b2d4e6e9
Snow <snow@Snow.localdomain>
2025-11-27 01:39:27 +0100

    Update

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: snowp <snowp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 00:02:35 by snowp             #+#    #+#             */
-/*   Updated: 2025/11/27 01:36:31 by snowp            ###   ########.fr       */
+/*   Updated: 2025/11/27 01:38:24 by snowp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,11 @@ static	size_t	ft_puthex(const unsigned long n)
 
 size_t	ft_putptr(void *ptr)
 {
+	size_t	count;
+
+	count = 0;
 	if (!ptr)
 		return (ft_putstr("(nil)"));
-	ft_putstr("0x");
-	return (ft_puthex((unsigned long)ptr) + 2);
+	count += ft_putstr("0x");
+	return (count + ft_puthex((unsigned long)ptr));
 }
